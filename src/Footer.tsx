87dec74b203825477f26591706a5aec5896ff8bb
Eddie Zieleniewski <solesskidder1@gmail.com>
2024-06-22 00:46:46 -0400

    Add footer

---
+++ src/Footer.tsx
@@ -0,0 +1,19 @@
+
+export const Footer = () => {
+
+  
+    return (
+    <div className="px-[30%] m-2 sm:px-[20%]">
+        <div className="flex justify-evenly items-center md:text-3xl sm:text-2xl">
+
+            <a target="_blank" href="mailto:solesskidder1@gmail.com" rel="noreferrer">
+                <button  className="text-[#b8d1fa]  outline-[#093172] text-[14px] font-bold">Contact Me</button>
+            </a>
+            <a target="_blank " href="https://github.com/soleq1/" rel="noreferrer">
+                <img src="/public/github-mark.png" alt="Github" className="w-[30px] h-[30px]" />
+            </a>
+          
+        </div>
+     </div>   
+)
+}
